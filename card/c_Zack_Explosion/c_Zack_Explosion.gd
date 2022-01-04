00744d942445144b3b0d091732f719bc4b30e04e
Zack <q1593572486@126.com>
2022-01-04 15:34:26 +0800

    慧慧技能调整

--- card/c_Zack_Explosion/c_Zack_Explosion.gd
+++ card/c_Zack_Explosion/c_Zack_Explosion.gd
@@ -1,25 +1,27 @@
 extends Tsk
 
 func init():
-	att.cry = 2
+	att.cry = 9
 	lv = 1
 	camp = "m_meihaoshijie"
 	name = "Explosion"
-	dec = "遮蔽光明的漆黑啊，怀抱黑夜的爆炎啊，吾以红魔之名，宣言原初之崩坏，在终末王国之地，隐藏力量根源之人，于吾眼前集结吧。Explosion!"
+	dec = "Explosion：先对敌方后排随从造成6点伤害，再对敌方所有随从造成3点伤害。损失三点法力水晶。"
 	aiTab = "we_hero_buff_n:1_p:1"
-
-func useSelIf(card,cell):
-	if card != null && card == getHero():
-		return true
-	return false
 
 func _use(card,cell):
 	if card != null: 
-		for i in getHero().lib.items:
-			if i.type == TYPE.CREATURE :
-				getHero().lib.eraseCard(i)
-				getHero().hand.addCard(i)
-				i.att.cry -= 2
-				if i.att.cry < 0: 
-					i.att.cry = 0
-				break
+		
+		# 先对后排随从打6
+		for i in Scene.getAllCard(getHero(),1,false):
+			hurtCard(i,6,true)
+		
+		# 再对全场随从打3
+		for i in Scene.getAllCard(getHero(),1,false):
+			hurtCard(i,3,true)
+		
+		var hero:Hero = getHero()
+		hero.setCry(hero.getCry()-3)
+		
+
+
+
