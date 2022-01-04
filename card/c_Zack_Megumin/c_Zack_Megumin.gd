3774606bada84edddf1fe00673a90b034a9df09d
Zack <q1593572486@126.com>
2022-01-04 21:52:42 +0800

    和真初始化

--- card/c_Zack_Megumin/c_Zack_Megumin.gd
+++ card/c_Zack_Megumin/c_Zack_Megumin.gd
@@ -9,7 +9,7 @@ func init():
 	camp = "m_meihaoshijie"
 	name = "慧慧"
 	tab = "英雄 人类 大魔法师"
-	tskId = "c_Zack_Explosion"
+	tskId = "c_Zack_Theft"
 	dec = "红魔族第一的天才魔法师。生于穷困的家庭，因吃不饱饭而营养缺乏（萝莉平胸），甚至要靠抢“背负苍蓝雷电之人”（背负巨乳之人）悠悠的便当来解决吃饭问题，为了变成巨乳而希望成为大魔法师"
 	isInit = true  #非初始英雄
 	needUlock = false #需要外围解锁
