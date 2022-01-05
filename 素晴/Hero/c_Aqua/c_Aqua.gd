51c09979ceada77b26785188269f2ae36a736c9c
Zack <q1593572486@126.com>
2022-01-05 10:47:08 +0800

    阿库娅初始化

---
+++ 素晴/Hero/c_Aqua/c_Aqua.gd
@@ -0,0 +1,22 @@
+extends Hero
+
+func init():
+	att.atk = 0
+	att.arm = 5
+	att.cry = 7
+	lv = 4
+	type = TYPE.HERO
+	camp = "m_meihaoshijie"
+	name = "阿库娅"
+	tab = "英雄,女神,智障"
+	tskId = "c_fuhuo"
+	dec = "复活一个墓地的随从"
+	isInit = true
+	needUlock = false
+
+
+func _use(card,cell):
+ 	pass
+
+
+	
---
+++ 素晴/Tsk/c_fuhuo/c_fuhuo.gd
@@ -0,0 +1,23 @@
+extends Tsk
+
+func init():
+	att.cry = 3
+	lv = 1
+	camp = "m_meihaoshijie"
+	name = "复活"
+	dec = "复活一个墓地的随从。"
+	aiTab = "we_hero_buff_n:1_p:1"
+
+
+func _use(card,cell):
+    var cs = []
+    for i in getHero().gry.items:
+        if i.type == Card.TYPE.CREATURE:
+            cs.append(i)
+    if cs.size() > 0 :
+        var cell = getHero().getNullCell()
+        if cell != null:
+            var inx = sys.rndRan(0,cs.size()-1)
+            var c = cs[inx]
+            getHero().gry.eraseCard(c)
+            getHero().cardInBattle(c,cell,true)
